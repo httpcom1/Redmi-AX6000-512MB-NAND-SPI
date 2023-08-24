f0a4e6802cb746c0e874094f1cd4462121d59fa5
httpcom1 <70948277+httpcom1@users.noreply.github.com>
2023-08-24 21:24:31 +0800

    Update diy-part1.sh

--- firmware_build/diy-part1.sh
+++ firmware_build/diy-part1.sh
@@ -17,7 +17,6 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
 ## 修改image/mt7986.mk中的BLOCKSIZE := 128k为256k，PAGESIZE := 2048为4096，好像可有可无
 # sed -i '/mt7986a-xiaomi-redmi-router-ax6000-uboot/{n;n;n;n;s/128k/256k/;}' target/linux/mediatek/image/mt7986.mk
